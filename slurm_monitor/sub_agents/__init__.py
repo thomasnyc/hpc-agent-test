4225d3538f859089ee39410919173c6920a4f5b0
Thomas Leung <thomashk@google.com>
2026-04-28 19:20:47 +0000

    deployment setup

--- slurm_monitor/sub_agents/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Package containing the various specialized sub-agents for the Slurm HPC Monitor."""
-
-# Expose sub-agents here so they can be easily imported into agent.py
-# e.g., from .sub_agents import env_analyst_agent
-from .slurm_analyst.agent import env_analyst_agent
-
-# As you continue migrating other agents, you can expose them here:
-# from .data_analyst import data_analyst_agent
